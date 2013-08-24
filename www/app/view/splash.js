431b4c192e7320b5ebe54d83d180e4341047c728
Melissa Wiel <mswiel@g.cofc.edu>
2013-08-24 12:27:04 -0400

    Splash!!!

---
+++ www/app/view/splash.js
@@ -0,0 +1,9 @@
+Ext.define("seaturtle.view.splash", {
+    extend: 'Ext.Panel',
+	xtype: 'splash',
+
+    config: {
+		html: '<br><br><div align="center"><iframe src="resources/images/Seaturtle1.animproj_export/index.html" style="border: 20px solid #CBCGD6; -moz-border-radius: 5px;-webkit-border-radius: 5px">'
+
+		}
+});

db77123cbb5cb7519a1fa3b52cfbcd67ea0abe06
Melissa Wiel <mswiel@g.cofc.edu>
2013-08-24 12:44:16 -0400

    Densaugeo

--- www/app/view/splash.js
+++ www/app/view/splash.js
@@ -4,14 +4,13 @@ Ext.define("seaturtle.view.splash", {
 requires: [
         'Ext.TitleBar'
     ],
-    scroll: 'vertical',
     config: {
         items: [
             {
                 cls:'my-titlebar',
                 docked: 'top',
                 xtype: 'titlebar',
-                title: "Save Our Sea Turtles",
+                title: "",
                 styleHtmlContent: true,
                 scrollable: true,
                 layout: {
@@ -19,10 +18,7 @@ requires: [
                     align: 'center'
                 }
             },{
-                height:  1200,
-                layout: 'vbox',
-                scrollable: 'vertical',
-		html: '<br><br><div align="center"><iframe src="resources/images/Seaturtle1.animproj_export/index.html" style="border: 20px solid #CBCGD6; -moz-border-radius: 5px;-webkit-border-radius: 5px">'
+				html: '<br><br><div align="center"><iframe src="resources/images/seaturtleanim/index.html" style="border: 20px solid #CBCGD6; -moz-border-radius: 5px;-webkit-border-radius: 5px">'
 
 
             }

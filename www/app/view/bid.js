04a2d7322564befbe8c864b32d1a4815a2523102
bjameson <brent@jamesonnetworks.com>
2013-08-24 12:44:09 -0400

    bid work

--- www/app/view/bid.js
+++ www/app/view/bid.js
@@ -51,7 +51,7 @@ Ext.define('seaturtle.view.bid', {
         },
         items: [
             {
-                title: ' ',
+                title: 'Save Our Sea Turtles',
                 style: 'background-color:#FFF',
                 styleHtmlContent: true,
                 scrollable: true,
@@ -61,17 +61,41 @@ Ext.define('seaturtle.view.bid', {
                 },
                 items: [{
                         styleHtmlContent: true,
-                        style: '',
-                        html: ["<div>",
-                        "<table border=\"1\" cellpadding=\"3\">",
-                        "<thead>",
-                            "<th>Name</th>",
-                            "<th>Amount</th>",
-                        "</thead>",
-                        "<tbody>",
-                        "<div id=\"rows\"></div>",
-                        "</tbody></table>",
-                        "</div>"].join(''),
+                        style: 'background-color:transparent',
+                        html: [
+<h3>Look for tracks</h3>
+<p>Dawn is the best time for finding sea turtle tracks. The females have come ashore during the previous night, creating the distinctive &ldquo;tractor&rdquo; marks as they pull themselves across the sand in search of a nesting site.</p>
+<p>But don&rsquo;t look for tracks near the water&rsquo;s edge. Find the mark of the last high tide and look between that &ldquo;guideline&rdquo; and the dry sand toward the dune line (or in Folly&rsquo;s case, toward the houses).</p>
+<p>If you see tracks, take care to avoid walking on the crawl or on a suspected nest site, note your landmarks (e.g., what block are you in?, what&rsquo;s the address or size/color of the nearest house?, what walkovers are nearby?), and&nbsp;<strong>call Folly Beach Public Safety at 843-588-2433</strong>. They will contact the Turtle Crew to document and assess the crawl and determine if eggs were laid.</p>
+<p>&nbsp;</p>
+<h3>Clear the beach</h3>
+<p>Remove all debris from the beach, especially plastics (e.g., cups, bottles, bags, fishing line). This will prevent them from being swept into the ocean by winds or high tides and mistaken as a food source by sea turtles, dolphin, and fish.</p>
+<p>Remove furniture from the beach to prevent nesting loggerheads from becoming entangled in chairs.</p>
+<p>Fill in holes created by &ldquo;kids&rdquo; at play. This will enable the tiny hatchlings to speed along their way to the ocean without getting trapped or disoriented. Watching a hatchling struggle to crawl out of a footprint increases awareness of their point of view and their obstacle-filled course.</p>
+<p>&nbsp;</p>
+<h3>Allow hatchlings to crawl</h3>
+<p>If you&rsquo;re fortunate enough to see hatchlings emerge from their nest, you&rsquo;ll surely be tempted to pick them up and carry them to the ocean. Stop yourself! It&rsquo;s vital that the hatchlings &ldquo;imprint&rdquo; on their natal beach. When the female hatchlings mature, they&rsquo;ll return to the same beach to nest. If you see hatchlings crawling off-course, simply guide them on a path to the ocean and cheer them on.&nbsp;<strong>Report any hatchling emergence or crawl activity by calling 843-588-2433</strong>.</p>
+<p>&nbsp;</p>
+<h3>Practice &amp; Preach &ldquo;Lights out for Turtles!&rdquo;</h3>
+<p>Artificial lights affect the nesting and hatching of sea turtles. Brightly lit beaches are a deterrent to nesting females and a deathtrap for hatchlings. Instead of being attracted by the natural light of the moon and starlight on the surf, hatchlings move toward the brighter beams of streetlights, porch &amp; deck lights, floodlights, interior lights, car headlights, flashlights, etc. They fail to find their way to the ocean quickly and become the victims of ghost crabs and gulls, or die from dehydration in the morning sun.&nbsp;<strong>Dark beaches are critical to sea turtle survival.</strong>&nbsp;Help educate others about the importance of &ldquo;lights out for sea turtles!&rdquo;</p>
+<p>&nbsp;</p>
+<h3>Report nest abuse</h3>
+<p>Occasionally dogs, cats, racoons, and careless humans will dig into a nest. If you notice that a newly laid or a relocated nest has been disturbed in any way,&nbsp;<strong>please report it to Folly Beach Public Safety by calling 843-588-2433.</strong></p>
+<p>Loggerhead turtles are a threatened species on the Endangered List and are protected by state and federal laws. Nests that have been relocated by licensed members of the Turtle Crew will be clearly marked<br />
+by a bright orange sign and bright plastic tape wound around wooden stakes that are numbered and dated. Monitoring the nests during the hatchlings&rsquo; 45/60-day incubation period is an important part of Folly&rsquo;s conservation program.</p>
+<p>&nbsp;</p>
+
+<h3>Celebrate the species</h3>
+
+<p>Sea turtles are ancient reptiles from the days of the dinosaurs. They are symbols of perseverance, longevity and eternity. Adult loggerheads are about 3 feet long and can weigh over 300 pounds. Females return after more than 20 years to nest on the beach where they were born, but males rarely come ashore.</p>
+
+<p>During the May-October nesting season, females may nest 2-5 times and lay approximately 100 ping-pong ball sized eggs in each nest. Hatching occurs, usually en masse, after 45-60 days. Loggerhead hatchlings are dark brown, about 2 inches long, and weigh only ounces.</p>
+
+<p>It&rsquo;s estimated that 1 out of 1000 hatchlings lives to reproduce. Their predators include ghost crabs, racoons, sea birds, fish, and especially human activities &mdash; egg and meat consumption, long-line fishing, TED-less trawling, careless boating, beachfront development.</p>
+
+<p>Celebrate the species &hellip;&nbsp;<strong>participate in its survival.</strong></p>
+
+                        ].join(''),
                         handler: function(event){
                         }
                     },
@@ -82,59 +106,19 @@ Ext.define('seaturtle.view.bid', {
                         width: 200,
                         margin:'0 0 0 -100px',
                         left:'50%',
-                        position: 'relative',
+                        position: 'absolute',
                         bottom: '20px',
                         height: '50px',
-                        float: 'center',
+                        float: 'left',
                         handler: function(btn){
                              popup.showBy(btn);
                         }
-                    },
-                    // {
-                    //     xtype: 'button',
-                    //     text: 'Bid',
-                    //     iconAlign: 'right',
-                    //     width: 200,
-                    //     margin:'0 0 0 -100px',
-                    //     left:'50%',
-                    //     position: 'relative',
-                    //     bottom: '20px',
-                    //     height: '50px',
-                    //     float: 'center',
-                    //     handler: function(btn){
-                    //          //formPanel.show();
-                    //     }
-                    // }
+                    }
                 ]
             }
         ]
     }
 });
-
-// var formPanel = new Ext.Create('Ext.form.Panel', {
-//     fullscreen: true,
-
-//     items: [{
-//         xtype: 'fieldset',
-//         items: [
-//             {
-//                 xtype: 'textfield',
-//                 name : 'name',
-//                 label: 'Name'
-//             },
-//             {
-//                 xtype: 'emailfield',
-//                 name : 'email',
-//                 label: 'Email'
-//             },
-//             {
-//                 xtype: 'passwordfield',
-//                 name : 'password',
-//                 label: 'Password'
-//             }
-//         ]
-//     }]
-// });
 
 var popup = new Ext.Panel({
     floating: true,

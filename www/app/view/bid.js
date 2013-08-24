14e0ddd3b7a8db224aa0a3c3b8caa9bd8c4df21f
bjameson <brent@jamesonnetworks.com>
2013-08-24 12:02:39 -0400

    addingbids

--- www/app/view/bid.js
+++ www/app/view/bid.js
@@ -1,3 +1,36 @@
+var bids = {
+    bids: [
+        {
+            name: "Cynthia Donatemuch",
+            amount: "100"
+        },
+        {
+            name: "John Q. Generous",
+            amount: "80"
+        },
+        {
+            name: "Steve Gives",
+            amount: "75"
+        },
+        {
+            name: "Bill Gates",
+            amount: "70"
+        }
+    ]
+}
+
+var bidTemp = "<tr><th>" +  + "</th><th>" +  + "</th></tr>";
+
+function getBids() {
+    var returnString = "";
+    for(var i = 0; i < bid.length; i++) {
+        returnString += "<tr><th>" + bid[i].name + "</th><th>" + bid[i].amount + "</th></tr>";
+        if (i == bid.length-1) {
+            return returnString;
+        }
+    }
+}
+
 Ext.define('seaturtle.view.bid', {
     extend: 'Ext.tab.Panel',
     xtype: 'bids',
@@ -24,8 +57,17 @@ Ext.define('seaturtle.view.bid', {
                 },
                 items: [{
                     styleHtmlContent: true,
-        style: 'align: center; background: #D2EDAF; border: 5px solid #A3D184;-moz-border-radius: 5px;-webkit-border-radius: 5px; padding:10px; height:93%; width:95%; margin:2.5%',
-                    html: '<div align="center"><iframe src="resources/images/Seaturtle1.animproj_export/index.html"frameBorder="0">',
+                    style: '',
+                    html: ["<div>",
+                    "<table border=\"1\" cellpadding=\"3\">",
+                    "<thead>",
+                        "<th>Name</th>",
+                        "<th>Account</th>",
+                    "</thead>",
+                    "<tbody>",
+                    "<div id=\"rows\"></div>",
+                    "</tbody></table>",
+                    "</div>"].join('')
                 },
                     {
                         xtype: 'button',
@@ -48,53 +90,49 @@ Ext.define('seaturtle.view.bid', {
     }
 });
 
-
-
-
-
-        var popup = new Ext.Panel({
-            floating: true,
-            modal: true,
-            width: 600,
-            height: 500,
-            styleHtmlContent: true,
-            html: [
-                    '<div style="margin-left:20px; margin-top:10px;">',
-                    '<form>',
-                    'First Name: <input type="text" style="width:400px;" name="Name"><br>',
-                    'Last Name:  <input type="text" style="width:401px;" name="Name"><br>',
-                    'Phone:      <input type="text" style="width:430px;" name="Name"><br>',
-                    'Email:      <input type="text" style="width:436px;" name="Name"><br>',
-                    'Address:    <input type="text" style="width:417px;" name="Name"><br>',
-                    'City:       <input type="text" style="width:446px;" name="Name"><br>',
-                    'State:      <input type="text" style="width:437px;" name="Name"><br>',
-                    'Zip Code:   <input type="text" style="width:412px;" name="Name"><br><br>',
-                    'Credit Card Type:<br>',
-                    '<ul',
-                    '<li><input type="radio" name="issue" value="Critial"> Visa<br></li>',
-                    '<li><input type="radio" name="issue" value="Moderate"> American Express<br></li>',
-                    '<li><input type="radio" name="issue" value="Minor"> Master Card<br><br></li>',
-                    'Credit Card Number:<br><input type="text" style="width:500px;" name="Name"><br><br>',
-                    '</form>',
-                    '</div>'
-                ].join(''),
-            dockedItems: [{
-                xtype: 'toolbar',
-                docked: 'bottom',
-                
-                items: [{
-                    text: 'Donate',
-                    handler: function(){
-                        popup.hide();
-                    }
-                },
-                { xtype: 'spacer' },
-                {
-                    text: 'Cancel',
-                    handler: function(){
-                        popup.hide();
-                    }
-                },
-                ]
-            }]
-        });
+var popup = new Ext.Panel({
+    floating: true,
+    modal: true,
+    width: 600,
+    height: 500,
+    styleHtmlContent: true,
+    html: [
+            '<div style="margin-left:20px; margin-top:10px;">',
+            '<form>',
+            'First Name: <input type="text" style="width:400px;" name="Name"><br>',
+            'Last Name:  <input type="text" style="width:401px;" name="Name"><br>',
+            'Phone:      <input type="text" style="width:430px;" name="Name"><br>',
+            'Email:      <input type="text" style="width:436px;" name="Name"><br>',
+            'Address:    <input type="text" style="width:417px;" name="Name"><br>',
+            'City:       <input type="text" style="width:446px;" name="Name"><br>',
+            'State:      <input type="text" style="width:437px;" name="Name"><br>',
+            'Zip Code:   <input type="text" style="width:412px;" name="Name"><br><br>',
+            'Credit Card Type:<br>',
+            '<ul',
+            '<li><input type="radio" name="issue" value="Critial"> Visa<br></li>',
+            '<li><input type="radio" name="issue" value="Moderate"> American Express<br></li>',
+            '<li><input type="radio" name="issue" value="Minor"> Master Card<br><br></li>',
+            'Credit Card Number:<br><input type="text" style="width:500px;" name="Name"><br><br>',
+            '</form>',
+            '</div>'
+        ].join(''),
+    dockedItems: [{
+        xtype: 'toolbar',
+        docked: 'bottom',
+        
+        items: [{
+            text: 'Donate',
+            handler: function(){
+                popup.hide();
+            }
+        },
+        { xtype: 'spacer' },
+        {
+            text: 'Cancel',
+            handler: function(){
+                popup.hide();
+            }
+        },
+        ]
+    }]
+});

310dd79ab0c37c90751ba275d458babd149c5a94
bjameson <brent@jamesonnetworks.com>
2013-08-24 12:23:33 -0400

    bids

--- www/app/view/bid.js
+++ www/app/view/bid.js
@@ -31,6 +31,10 @@ function getBids() {
     }
 }
 
+function addBid(bid) {
+    bids.add(bid);
+}
+
 Ext.define('seaturtle.view.bid', {
     extend: 'Ext.tab.Panel',
     xtype: 'bids',
@@ -56,19 +60,21 @@ Ext.define('seaturtle.view.bid', {
                     align: 'center'
                 },
                 items: [{
-                    styleHtmlContent: true,
-                    style: '',
-                    html: ["<div>",
-                    "<table border=\"1\" cellpadding=\"3\">",
-                    "<thead>",
-                        "<th>Name</th>",
-                        "<th>Account</th>",
-                    "</thead>",
-                    "<tbody>",
-                    "<div id=\"rows\"></div>",
-                    "</tbody></table>",
-                    "</div>"].join('')
-                },
+                        styleHtmlContent: true,
+                        style: '',
+                        html: ["<div>",
+                        "<table border=\"1\" cellpadding=\"3\">",
+                        "<thead>",
+                            "<th>Name</th>",
+                            "<th>Amount</th>",
+                        "</thead>",
+                        "<tbody>",
+                        "<div id=\"rows\"></div>",
+                        "</tbody></table>",
+                        "</div>"].join(''),
+                        handler: function(event){
+                        }
+                    },
                     {
                         xtype: 'button',
                         text: 'Donate',
@@ -83,11 +89,51 @@ Ext.define('seaturtle.view.bid', {
                         handler: function(btn){
                              popup.showBy(btn);
                         }
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Bid',
+                        iconAlign: 'right',
+                        width: 200,
+                        margin:'0 0 0 -100px',
+                        left:'50%',
+                        position: 'absolute',
+                        bottom: '20px',
+                        height: '50px',
+                        float: 'center',
+                        handler: function(btn){
+                             formPanel.show();
+                        }
                     }
                 ]
             }
         ]
     }
+});
+
+var formPanel = new Ext.Form('Ext.form.Panel', {
+    fullscreen: true,
+
+    items: [{
+        xtype: 'fieldset',
+        items: [
+            {
+                xtype: 'textfield',
+                name : 'name',
+                label: 'Name'
+            },
+            {
+                xtype: 'emailfield',
+                name : 'email',
+                label: 'Email'
+            },
+            {
+                xtype: 'passwordfield',
+                name : 'password',
+                label: 'Password'
+            }
+        ]
+    }]
 });
 
 var popup = new Ext.Panel({
@@ -99,20 +145,17 @@ var popup = new Ext.Panel({
     html: [
             '<div style="margin-left:20px; margin-top:10px;">',
             '<form>',
-            'First Name: <input type="text" style="width:400px;" name="Name"><br>',
-            'Last Name:  <input type="text" style="width:401px;" name="Name"><br>',
-            'Phone:      <input type="text" style="width:430px;" name="Name"><br>',
-            'Email:      <input type="text" style="width:436px;" name="Name"><br>',
-            'Address:    <input type="text" style="width:417px;" name="Name"><br>',
-            'City:       <input type="text" style="width:446px;" name="Name"><br>',
-            'State:      <input type="text" style="width:437px;" name="Name"><br>',
-            'Zip Code:   <input type="text" style="width:412px;" name="Name"><br><br>',
+            'First Name: <input type="text" style="width:400px;" name="FirstName"><br>',
+            'Last Name:  <input type="text" style="width:401px;" name="LastName"><br>',
+            'Phone:      <input type="text" style="width:430px;" name="Phone"><br>',
+            'Email:      <input type="text" style="width:436px;" name="Email"><br>',
+            'Zip Code:   <input type="text" style="width:412px;" name="ZipCode"><br><br>',
             'Credit Card Type:<br>',
             '<ul',
-            '<li><input type="radio" name="issue" value="Critial"> Visa<br></li>',
-            '<li><input type="radio" name="issue" value="Moderate"> American Express<br></li>',
-            '<li><input type="radio" name="issue" value="Minor"> Master Card<br><br></li>',
+            '<li><input type="radio" name="issue" value="visa"> Visa<br></li>',
+            '<li><input type="radio" name="issue" value="amex"> American Express<br></li>',
             'Credit Card Number:<br><input type="text" style="width:500px;" name="Name"><br><br>',
+            'Amount:<br><input type="text" style="width:500px;" name="amount"><br><br>',
             '</form>',
             '</div>'
         ].join(''),
@@ -122,7 +165,9 @@ var popup = new Ext.Panel({
         
         items: [{
             text: 'Donate',
-            handler: function(){
+            handler: function(event, event2, event3, event4, event5){
+                //debugger;
+                //addBid({ name: event.FirstName, amount: event.amount });
                 popup.hide();
             }
         },
